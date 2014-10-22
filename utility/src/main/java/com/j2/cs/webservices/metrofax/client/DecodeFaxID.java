426ac1aff3b47cc9d1d34a2adedcdd2466583b4b
christian.banzon <christian.banzon@J2SDENG008.j2global.com>
2014-10-22 11:36:46 -0700

    test again

--- utility/src/main/java/com/j2/cs/webservices/metrofax/client/DecodeFaxID.java
+++ utility/src/main/java/com/j2/cs/webservices/metrofax/client/DecodeFaxID.java
@@ -40,7 +40,17 @@ public class DecodeFaxID extends Composite implements HasText {
 	interface DecodeFaxIDUiBinder extends UiBinder<Widget, DecodeFaxID> {
 	}
 
-	
+	/**
+	 * Because this class has a default constructor, it can
+	 * be used as a binder template. In other words, it can be used in other
+	 * *.ui.xml files as follows:
+	 * <ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
+	 *   xmlns:g="urn:import:**user's package**">
+	 *  <g:**UserClassName**>Hello!</g:**UserClassName>
+	 * </ui:UiBinder>
+	 * Note that depending on the widget that is used, it may be necessary to
+	 * implement HasHTML instead of HasText.
+	 */
 	public DecodeFaxID(UtilityServiceAsync service) {
 		this.service = service;
 		initWidget(uiBinder.createAndBindUi(this));
